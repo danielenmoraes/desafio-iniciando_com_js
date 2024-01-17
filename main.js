c12c9cc90b8831b8ad42c82e3a0a16a701fd8587
Danielen Moraes <138619584+danielenmoraes@users.noreply.github.com>
2024-01-17 13:20:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,130 @@
+/* 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+    let mensagem = "Hello Word!"
+   alert(mensagem)
+*/ 
+
+
+/* 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+    
+    let nome = "Danielen"
+    let sobrenome = "Moraes"
+    alert(nome + sobrenome)
+*/ 
+
+
+
+
+/* 3. Crie um script que declare uma variável e verifique se o seu valor é um número. 
+    Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+
+    const valor = prompt("Digite um número: ");
+
+    if (isNaN(valor)) {
+    alert("Não é um número");
+    } else {
+    alert("É um número.");
+    }
+*/
+
+/* 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+    Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+    const valor = "5"
+    console.log(typeof valor)
+
+    if (typeof valor == "string") {
+    alert("É uma string");
+    } else {
+    alert("Não é uma string.");
+}
+*/
+    
+/* 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+    Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+    let valor = false
+
+    if (typeof valor === "boolean") {
+    alert("É um booleano");
+    } else {
+    alert("Não é um booleano");
+    }
+    console.log(typeof valor)
+
+
+*/
+
+
+/* 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+    let number1 = 10
+    let number2 = 2
+
+    alert(`A soma dos dois números é igual a :${number1 + number2}`)
+
+
+
+*/
+
+/* 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+    let number1 = 10
+    let number2 = 2
+
+    alert(`A multiplicação dos dois números é igual a :${number1 * number2}`)
+
+*/
+
+/* 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+    let number1 = 10
+    let number2 = 2
+
+    alert(`A divisão entre os dois números é igual a :${number1 / number2}`)
+ */
+
+
+/* 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+    Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+    let num1 = prompt("Digite um número")
+
+    
+    let total = Number(num1)
+    let resultado = total%2 == 0;
+    
+    
+    
+    if (resultado){
+    
+        alert('É um número par')
+    } else{
+        alert('Não é um número par')
+    }
+ */
+
+  /* 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar.
+     Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+
+    let num1 = prompt("Digite um número")
+
+    
+    let total = Number(num1)
+    let resultado = total%2 == 1;
+    
+    
+    
+    if (resultado){
+    
+        alert('É um número impar')
+    } else{
+        alert('Não é um número impar')
+    }
+  
+  */
+
+
+
